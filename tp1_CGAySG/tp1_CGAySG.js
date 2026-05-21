74bd9f5d26b11b119f544fbb617137a32fd11d4c
tatievelym <montenegrotatianaevelyn@gmail.com>
2026-05-20 21:19:14 -0300

    class figura duplicado/borrado

--- tp1_CGAySG/tp1_CGAySG.js
+++ tp1_CGAySG/tp1_CGAySG.js
@@ -44,7 +44,7 @@ function setup() {
 
 function draw() {
 
-  background('#4F6EA0');
+  background(127);
 
   let ahora = millis();
 
@@ -282,43 +282,5 @@ function keyReleased() {
     for (let f of figuras) {
       f.escalando = false;
     }
-  }
-}
-
-class Figura 
-  constructor(id, img, x, y, tam) {
-    this.id = id;
-    this.img = img;
-    this.x = x;
-    this.y = y;
-    this.tam = tam;
-    this.anguloActual = 0;
-    this.anguloOrigen = 0;
-    this.velocidadGiro = 0;
-    this.girando = false;
-    this.tiempoInicio = 0;
-    this.escalaActual = 1.0;
-    this.escalando = false;
-    this.desplazX = 0;
-    this.desplazY = 0;
-    this.arrastrada = false;
-  }
-
-  dibujar() {
-    push();
-    translate(
-      this.x + this.desplazX,
-      this.y + this.desplazY
-      );
-    rotate(this.anguloActual);
-    scale(this.escalaActual);
-    image(
-      this.img,
-      0,
-      0,
-      this.tam,
-      this.tam
-      );
-    pop();
   }
 }
